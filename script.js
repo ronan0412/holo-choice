36eec7a2358d314a25d01e2eba3f76b14e578e4f
Kosuke <darthbenirece@gmail.com>
2023-12-13 00:04:58 +0900

    script.js is fixed..

--- script.js
+++ script.js
@@ -1,79 +1,57 @@
 'use strict';
-//ok Todo startを２回押したときの回避が必要
-//ok Todo 更新押した時にすぐ実行されてしまうのを回避
-//ok Todo stop 押した後にintervalIDを受け渡し-->グローバルにしたい
-//ok start-->stop-->startの時に再度startさせたい
-
 /*
-スタートボタンを押す
-ホロライブメンバーの数だけ数字に合わせたホロメンの立ち絵がルーレットのように表示される (onclick())
-ストップボタンを押すと、そのホロメンの紹介が表示される
+###期待動作###
+1) スタートボタンを押す
+2) ホロライブメンバーの数だけ立ち絵がルーレットのように表示される
+3) ストップボタンを押すと、そのホロメンの紹介が表示される
 */
 
-//動作確認
-//let intervalID = setInterval(randomHolomem, 100);
-
-let b = 0;//はじめて読み込んだ時
+let done = 0;
 let intervalID = 0;
+// 1) スタートボタンを押す
 function start() {
-    //ifでstart押し済みかどうか判定？-->済みなら1でcontinueで重ね実行させない
-    if (b === 0) {//読み込んだ後初回の実施
-        intervalID = setInterval(randomHolomem, 100);
-        console.log("IDは" + intervalID);
-        //onclickの後に別途intervalIDに代入すると、もう一回setIntervalが呼び出される
-        b = 1;//実行済み
-        console.log("初回の実行");//Todo start-->stop-->start
-            document.getElementById("holomem-greeting").innerHTML = "";
-    document.getElementById("holoname-jp").innerHTML = "";
-    document.getElementById("holoname-en").innerHTML = "";
+    //1回目の呼び出し
+    if (done === 0) {
+        // "2)" を100ミリ秒間隔で実行することで、ルーレットのように表示させる
+        intervalID = setInterval(randomHolomem, 100);//n > 0
+        done = 1;//実行済み
+        console.log("初回の実行");
+        //ルーレットのように立ち絵を表示する間は名前などを表示させない
+        document.getElementById("holomem-greeting").innerHTML = "";
+        document.getElementById("holoname-jp").innerHTML = "";
+        document.getElementById("holoname-en").innerHTML = "";
+    //2回目以降の呼び出し
     } else {
-        console.log("2回目以降の実行"+b);
-    //2回目以降呼び出し時に値を削除する
-    document.getElementById("holomem-greeting").innerHTML = "";
-    document.getElementById("holoname-jp").innerHTML = "";
-    document.getElementById("holoname-en").innerHTML = "";
-    return;
+        console.log("2回目以降の実行");
+        return;//2回目以降スタートを押された場合は処理を終わらせる
     }
 }
 
-//ランダムにホロメンをピックする
 let i = 0;
+// 2) ランダムに立ち絵を表示させる
 function randomHolomem() {
     i = Math.floor(Math.random() * holomem.length);
     document.getElementById("holomem-picture").src = holomem[i].image;
     return i;
 }
-//ストップボタンを押したら、アニメーションのホロメンをピックする、それを表示する
+
 let pickedIndex = 0;
+// 3) ストップボタンを押す
 function pickHolomem() {
-    //Todo:この関数が実行された時に、STARTを押した後であることを判定してから以下を実行
-    //ok? Todo:多分一度START-->STOP-->STOPを押された場合も考慮
-    //ok Todo:random関数をまず止める clearIntervalで、インターバルID
-    //console.log("IDは" + start().intervalID);
-    console.log("IDは" + intervalID);
+    // "2)" の繰り返しを停止する
     clearInterval(intervalID);
-
-    //ok Todo stopを押しただけでランダムで1人表示される不具合を修正する ため、ifで「setInterval中かどうか」分岐
-    //1 動かす 2 idある 3 stop押す-->clear 4
+    // ストップボタンが押され "2)" の繰り返しが停止している場合
     if (intervalID === 0) {
-        return;
+        return;//2回目以降ストップを押された場合は処理を終わらせる
+    // スタートボタンを押して "2)" の繰り返しが続いている場合
     } else if (intervalID !== 0 && typeof intervalID === "number") {
-        //intervalIDが0ではなく、かつデータ型が数字の場合
-        pickedIndex = randomHolomem();
-        console.log(randomHolomem());
-        console.log(holomem[pickedIndex]);
-
+        pickedIndex = randomHolomem();//ランダムに選ばれたホロメンのindex
+        //選ばれたホロメンの画像や名前などを表示する
         document.getElementById("holomem-picture").src = holomem[pickedIndex].image;
         document.getElementById("holomem-greeting").innerHTML = "「" + holomem[pickedIndex].greetings + "」";
         document.getElementById("holoname-jp").innerHTML = holomem[pickedIndex].JPname;
         document.getElementById("holoname-en").innerHTML = holomem[pickedIndex].ENname;
     }
-
-    console.log("IDは消えた？" + intervalID);
-    intervalID = 0;
-    console.log("IDは消した？" + intervalID);
-    b = 0;//stopしたら次にスタート押せるようにカウンターをリセット
-
-    //難しかったところとどういう方針で対処したかは聞かれそう
-    //一発でURL叩けば使える状態にしよう（pen?+gitpage)
+    intervalID = 0;//スタート-->ストップ-->ストップ...と押された場合を回避
+    done = 0;//スタート-->ストップ-->スタート...と押された場合を回避
 }
